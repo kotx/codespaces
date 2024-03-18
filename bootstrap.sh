cc854f2cb7bb3faa88aa18003ca670a853b91ec5
Kot C <kot@yukata.dev>
2024-03-18 01:13:33 -0500

    Install flyctl

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,4 @@ sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USER
 curl -fsSL https://sh.rustup.rs | bash -s -- -y
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.2
 curl -sfL https://raw.githubusercontent.com/ducaale/xh/master/install.sh | sh
+curl -L https://fly.io/install.sh | sh
