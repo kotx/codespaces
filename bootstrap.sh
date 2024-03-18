487a74edb6ac7fba5234e81b75a57f1733df7927
Kot C <kot@yukata.dev>
2024-03-18 01:14:40 -0500

    Add flyctl to bashrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,8 @@ curl -fsSL https://sh.rustup.rs | bash -s -- -y
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.2
 curl -sfL https://raw.githubusercontent.com/ducaale/xh/master/install.sh | sh
 curl -L https://fly.io/install.sh | sh
+
+cat <<EOF
+export FLYCTL_INSTALL="/home/codespace/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
+EOF >> $HOME/.bashrc
